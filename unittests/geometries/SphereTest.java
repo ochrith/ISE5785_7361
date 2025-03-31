58cf4885d05deae13c5330962c51ecd0281b855d
ochrith <perezclaire868@gmail.com>
2025-03-31 12:48:55 +0300

    change getnormal in sphere test

--- unittests/geometries/SphereTest.java
+++ unittests/geometries/SphereTest.java
@@ -21,46 +21,13 @@ class SphereTest {
     /**
      *  Test method for {@link geometries.Sphere#getNormal(primitives.Point)}.
      */
+
     @Test
-    void testGetNormal() {  Cylinder c = new Cylinder(6, new Ray(new Point(4, 0, 0), new Vector(1, 0, 0)), 2);
-
+    void testGetNormal() {
         // ============ Equivalence Partitions Tests ==============
-
-        Vector v1 = c.getNormal(new Point(7, -1, 0));
-        Vector v2 = c.getNormal(new Point(4, 0, 1));
-        Vector v3 = c.getNormal(new Point(10, 0, 1));
-
-        // TC01: Test that the normal is the right one
-        assertEquals(new Vector(0, -1, 0), v1, "getNormal() wrong result");
-
-        // TC02: Test that getNormal works for the ray base
-        assertEquals(new Vector(-1, 0, 0), v2, "getNormal() not working properly for the ray base");
-
-        // TC03: Test that getNormal works for the second base
-        assertEquals(new Vector(1, 0, 0), v3, "getNormal() not working properly for the second base");
-
-        // =============== Boundary Values Tests ==================
-
-        Vector v4 = c.getNormal(new Point(4, 0, 0));
-        Vector v5 = c.getNormal(new Point(10, 0, 0));
-        Vector v6 = c.getNormal(new Point(4, 1, 0));
-        Vector v7 = c.getNormal(new Point(10, 1, 0));
-
-        // TC04: Test that getNormal works for the center point of the ray base
-        assertEquals(new Vector(-1, 0, 0), v4, "getNormal() not working properly for the center point of the ray base");
-
-        // TC05: Test that getNormal works for the center point of the top base
-        assertEquals(new Vector(1, 0, 0), v5,//
-                "getNormal() not working properly for the center point of the second base");
-
-        // TC06: Test that getNormal works for the point on the edge of the ray base
-        assertEquals(new Vector(-1, 0, 0), v6,//
-                "getNormal() not working properly for a point on the edge of the ray base");
-
-        // TC07: Test that getNormal works for the point on the edge of the second base
-        assertEquals(new Vector(1, 0, 0), v7,//
-                "getNormal() not working properly for a point on the edge of the second base");
-
+        // TC01: Test for a proper result
+        Point p1 = new Point(1, 0, 0);
+        assertEquals(p1, new Sphere(1, p1).getNormal(new Point(2, 0, 0)), "Failed to get the normal vector of the sphere");
     }
 
 
