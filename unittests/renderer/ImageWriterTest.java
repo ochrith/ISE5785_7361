91b58da9f840bed3c0f2cbfe3ae10e37c04b9c12
ochrith <perezclaire868@gmail.com>
2025-05-05 16:33:32 +0300

    shlav5 - adding components

---
+++ unittests/renderer/ImageWriterTest.java
@@ -0,0 +1,24 @@
+package renderer;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ImageWriterTest {
+
+    @Test
+    void testNY() {
+    }
+
+    @Test
+    void testNX() {
+    }
+
+    @Test
+    void testWriteToImage() {
+    }
+
+    @Test
+    void testWritePixel() {
+    }
+}
