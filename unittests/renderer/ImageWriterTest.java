e7d13b9de81d156077c91640555104f4a256dc27
ochrith <perezclaire868@gmail.com>
2025-05-05 17:05:00 +0300

    Realization test in ImageWriterTest

--- unittests/renderer/ImageWriterTest.java
+++ unittests/renderer/ImageWriterTest.java
@@ -1,6 +1,7 @@
 package renderer;
 
 import org.junit.jupiter.api.Test;
+import primitives.Color;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,6 +17,18 @@ class ImageWriterTest {
 
     @Test
     void testWriteToImage() {
+        ImageWriter imageWriter = new ImageWriter(800, 500);
+
+        Color turquoise = new Color(26, 241,225);
+        Color grey = new Color(71, 90,89);
+        for(int i=0; i < imageWriter.nX(); i++)
+        {
+            for(int j=0; j < imageWriter.nY(); j++)
+            {
+                imageWriter.writePixel(i, j, i % 50 == 0 || j % 50 == 0 ? grey : turquoise);
+            }
+        }
+        imageWriter.writeToImage("firstImage");
     }
 
     @Test
