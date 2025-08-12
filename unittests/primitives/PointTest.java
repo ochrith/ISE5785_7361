a980e8fa48077d84b7daa0fd082ca0dfeed3ecfe
ochrith <perezclaire868@gmail.com>
2025-08-12 12:22:37 +0200

    Realization of PointTest

--- unittests/primitives/PointTest.java
+++ unittests/primitives/PointTest.java
@@ -2,7 +2,7 @@ package primitives;
 
 /**
  * Unit tests for primitives.Point class
- * @author Ochrith Perez and Guila Czerniewicz
+ * @author Ochrith Perez 
  */
 import org.junit.jupiter.api.Test;
 
