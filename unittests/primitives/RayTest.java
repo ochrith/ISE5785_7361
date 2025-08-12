d8b094306a41b0a2335b5314434fe4a800ac05a6
ochrith <perezclaire868@gmail.com>
2025-08-12 12:22:55 +0200

    Realization test in RayTest - testFindClosestPoint

--- unittests/primitives/RayTest.java
+++ unittests/primitives/RayTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Unit tests for primitives.Ray class
- * @author Ochrith Perez and Guila Czerniewicz
+ * @author Ochrith Perez 
  */
 
 class RayTest {
